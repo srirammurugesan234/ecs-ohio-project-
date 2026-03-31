ba6dec5f8ba9fdc226beac48320a8e314aab045e
sriram <srirammurugesan95@mail.com>
2026-03-31 12:14:36 +0530

    Updated app.js text for verification

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Success!</h1><p>Running on AWS ECS Fargate in Ohio 1.</p>');
+  res.send('<h1>Welcome!</h1><p>1st demo completed successfully.</p>');
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
