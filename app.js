d34c8b7bed473b68451ea08f5aa142040c0361f4
sriram <srirammurugesan95@mail.com>
2026-03-31 12:00:07 +0530

    Added [what you added] and updated [what you changed]

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Success!</h1><p>Running on AWS ECS Fargate in Ohio.</p>');
+  res.send('<h1>Success!</h1><p>Running on AWS ECS Fargate in Ohio 1.</p>');
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
