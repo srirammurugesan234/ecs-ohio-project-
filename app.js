f3d3931dcca5a1e484131b235eaca81d90a48f3d
sriram <srirammurugesan95@mail.com>
2026-04-01 16:07:45 +0530

    Updated app.js text for verification

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome!</h1><p>1st demo completed successfully.</p>');
+  res.send('<h1>Welcome!</h1><p>demo completed successfully.</p>');
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
